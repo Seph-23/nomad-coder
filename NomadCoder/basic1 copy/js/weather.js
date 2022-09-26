52e66e090a5cb40f06b15e86ff28879269e07ff2
Seph-23 <seongmlyu95@gmail.com>
2022-09-26 14:28:53 +0900

    .

---
+++ NomadCoder/basic1 copy/js/weather.js
@@ -0,0 +1,15 @@
+const API_KEY = e5d8c9e3ba7d41ea6f204a11fc9bf6b6;
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live in", lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+  console.log(url);
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
